01006c02551b7ee386a7e1b05fe1192275c78555
Amir Saeid Dehghan <108151841+saaeiddev@users.noreply.github.com>
2026-09-24 12:35:04 +0330

    Refresh offline cache for new My Music feature

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'my-health-static-v1';
+const CACHE = 'my-health-static-v2';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./icon.svg','./manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
