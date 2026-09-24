bac9ed685238ff2c24b72d6654ed6b2d183976a4
Amir Saeid Dehghan <108151841+saaeiddev@users.noreply.github.com>
2026-09-24 13:02:37 +0330

    Release ten complete lifestyle pages and refresh offline app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'my-health-static-v4';
+const CACHE = 'my-health-static-v5';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./extras.js','./icon.svg','./manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
